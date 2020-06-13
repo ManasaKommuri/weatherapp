762be2a8412407e86f54bf6d92cfb4d66dc0a801
afrozshaikh25 <afrozshaikh725@gmail.com>
2020-06-13 18:14:21 +0530

    Initial Commit

---
+++ lib/Screens/WelcomeScreen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+//Todo: add the welcome screen here
+
+class WelcomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+    );
+  }
+}
